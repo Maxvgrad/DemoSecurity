17cf9e9898e5f1087a9381a3c293677034eb716c
Maksim Vinogradov <Maxvgrad@gmail.com>
2018-10-18 21:07:49 +0300

    Init commit

---
+++ src/main/java/ru/security/demo/controller/LoginController.java
@@ -0,0 +1,12 @@
+package ru.security.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+
+    public String sayHello() {
+        return "Hello";
+    }
+}
